26cec5db9de11dffbe14dda35144d1bbd904416e
richardyilin <richard19990101@gmail.com>
2023-11-21 14:42:20 -0800

    spring mass systme

--- src/kalman_filter.py
+++ src/kalman_filter.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import minimize
+from spring_mass import spring_mass
 
 def kalman_filter(observed_distance, time_intervals, process_variance, measurement_variance, initial_state):
     initial_distance = initial_state[0]
@@ -12,7 +13,7 @@ def kalman_filter(observed_distance, time_intervals, process_variance, measureme
     estimated_distance = []
     for i in range(len(observed_distance)):
         # Prediction step
-        delta_t = time_intervals[i] if i > 0 else 1.0  # Use 1.0 as a default for the first iteration
+        delta_t = time_intervals[i-1] if i > 0 else 1.0  # Use 1.0 as a default for the first iteration
         F = np.array([[1, delta_t], [0, 1]])  # State transition matrix
         Q = np.array([[0.5 * delta_t**2 * process_variance, delta_t * process_variance],
                       [delta_t * process_variance, process_variance]])  # Process covariance
@@ -29,10 +30,12 @@ def kalman_filter(observed_distance, time_intervals, process_variance, measureme
 
     return estimated_distance
 
+optimize = False
 np.random.seed(42)
-true_distance = np.linspace(0, 100, 100)
-observed_distance = true_distance + np.random.normal(0, 5, 100)
-time_intervals = np.random.uniform(0.5, 1.5, 100)
+true_distance, time_intervals = spring_mass()
+# true_distance = np.linspace(0, 100, 100)
+observed_distance = true_distance + np.random.normal(0, 5, len(true_distance))
+# time_intervals = np.random.uniform(0.5, 1.5, 100)
 initial_state = [observed_distance[0], 0.0] # [initial distance, initial relative velocity]
 def objective_function(params):
     process_variance, measurement_variance = params
@@ -42,20 +45,22 @@ def objective_function(params):
 
 initial_guess = [0.1, 5.0]
 bounds = [(1e-5, 1.0), (1e-5, 10.0)]
-result = minimize(objective_function, initial_guess, bounds=bounds)
-best_process_variance, best_measurement_variance = result.x
-estimated_distance_optimized = kalman_filter(observed_distance, time_intervals, best_process_variance, best_measurement_variance, initial_state)
 estimated_distance_unoptimized = kalman_filter(observed_distance, time_intervals, initial_guess[0], initial_guess[1], initial_state)
+if optimize:
+    result = minimize(objective_function, initial_guess, bounds=bounds)
+    best_process_variance, best_measurement_variance = result.x
+    estimated_distance_optimized = kalman_filter(observed_distance, time_intervals, best_process_variance, best_measurement_variance, initial_state)
 plt.figure(figsize=(12, 6))
-plt.subplot(1, 2, 1)
-plt.plot(true_distance, label='True Distance')
-plt.plot(observed_distance, label='Observed Distance', marker='o')
-plt.plot(estimated_distance_optimized, label='Optimized Estimated Distance', linestyle='--', marker='x')
-plt.legend()
-plt.xlabel('Time')
-plt.ylabel('Distance')
-plt.title('Optimized Kalman Filter for Distance Estimation')
-plt.subplot(1, 2, 2)
+if optimize:
+    plt.subplot(1, 2, 1)
+    plt.plot(true_distance, label='True Distance')
+    plt.plot(observed_distance, label='Observed Distance', marker='o')
+    plt.plot(estimated_distance_optimized, label='Optimized Estimated Distance', linestyle='--', marker='x')
+    plt.legend()
+    plt.xlabel('Time')
+    plt.ylabel('Distance')
+    plt.title('Optimized Kalman Filter for Distance Estimation')
+    plt.subplot(1, 2, 2)
 plt.plot(true_distance, label='True Distance')
 plt.plot(observed_distance, label='Observed Distance', marker='o')
 plt.plot(estimated_distance_unoptimized, label='Unoptimized Estimated Distance', linestyle='--', marker='x')
@@ -65,11 +70,12 @@ plt.ylabel('Distance')
 plt.title('Unoptimized Kalman Filter for Distance Estimation')
 plt.tight_layout()
 plt.show()
-print("Best Process Variance (Optimized):", best_process_variance)
-print("Best Measurement Variance (Optimized):", best_measurement_variance)
 mse_observed = np.mean((true_distance - observed_distance)**2)
-mse_optimized = np.mean((true_distance - estimated_distance_optimized)**2)
 mse_unoptimized = np.mean((true_distance - estimated_distance_unoptimized)**2)
 print("\nMean Squared Error (Observed Distance):", mse_observed)
-print("Mean Squared Error (Optimized Estimated Distance):", mse_optimized)
 print("Mean Squared Error (Unoptimized Estimated Distance):", mse_unoptimized)
+if optimize:
+    print("Best Process Variance (Optimized):", best_process_variance)
+    print("Best Measurement Variance (Optimized):", best_measurement_variance)
+    mse_optimized = np.mean((true_distance - estimated_distance_optimized)**2)
+    print("Mean Squared Error (Optimized Estimated Distance):", mse_optimized)
---
+++ src/spring_mass.py
@@ -0,0 +1,68 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+
+def spring_mass():
+    def car1_motion(t):
+
+        if t < car1_speed_up_time:
+            displacement_car1 = 0.5 * car1_acceleration * (t**2)
+        elif t >= car1_speed_up_time and t < car1_speed_up_time + car1_constant_time:
+            displacement_car1 = car1_velocity * t -  0.5 * car1_acceleration * (car1_speed_up_time**2)
+        else:
+            displacement_car1 = car1_velocity * t -  0.5 * car1_acceleration * (car1_speed_up_time**2) - 0.5 * car1_acceleration * ((t - car1_constant_time - car1_speed_up_time)**2)
+        return initial_distance + displacement_car1
+
+    def car2_spring_system(y, t, m2, k, initial_distance):
+        """
+        Define the differential equation for the rear car (car 2) in the spring-mass system.
+        y[0] is the displacement of car 2, y[1] is the velocity of car 2.
+        """
+        displacement_car1 = car1_motion(t)
+        displacement_car2 = y[0]
+
+        # Ensure that Car 2 cannot overtake Car 1
+        # assert np.all(displacement_car2 <= displacement_car1), "Car 2 cannot overtake Car 1"
+
+        dydt = [
+            y[1],                                 # Velocity of car 2
+            k * ((displacement_car1 - displacement_car2) - initial_distance) / m2  # Acceleration of car 2
+        ]
+        return dydt
+
+    # System parameters
+    m2 = 1000.0        # Mass of car 2
+    k = 100.0        # Spring constant
+    initial_distance = 40.0  # Initial distance between car 1 and car 2
+    # Initial conditions: displacement and velocity for car 2
+    initial_conditions_car2 = [0.0, 0.0]
+    # Time grid
+    car1_acceleration = 2.5
+    car1_velocity = 11
+    car1_constant_time = 60
+    car1_speed_up_time = car1_velocity / car1_acceleration
+    time = car1_constant_time + car1_speed_up_time * 2
+    t = np.linspace(0, time, 100000)
+
+    # Solve the differential equation for car 2
+    solution_car2 = odeint(car2_spring_system, initial_conditions_car2, t, args=(m2, k, initial_distance))
+
+    # Extract displacement and velocity of car 2 from the solution
+    displacement_car2 = solution_car2[:, 0]
+    velocity_car2 = solution_car2[:, 1]
+    displacement_car1 = []
+    for single_t in t:
+        displacement_car1.append(car1_motion(single_t))
+    # Plot the positions of car 1 (independent motion) and car 2 (spring-mass system) over time
+    # plt.figure(figsize=(8, 6))
+    # plt.plot(t, displacement_car1, label='Car 1 (front car)')
+    # plt.plot(t, displacement_car2, label='Car 2 (rear car)')
+    # plt.title('Two-Car System: Car 1 (Independent) and Car 2 (Spring-Mass)')
+    # plt.xlabel('Time (s)')
+    # plt.ylabel('Displacement (m)')
+    # plt.legend()
+    # plt.grid(True)
+    # plt.show()
+    true_distance = displacement_car1 - displacement_car2
+    time_intervals = [t[i + 1] - t[i] for i in range(len(t) - 1)]
+    return true_distance, time_intervals
