b0628b6c1566edbcbfa8aeab40f8a24e26596c28
richardyilin <richard19990101@gmail.com>
2023-11-23 19:21:29 -0800

    input is a scalar not a list

--- src/kalman_filter.py
+++ src/kalman_filter.py
@@ -3,53 +3,74 @@ import matplotlib.pyplot as plt
 from scipy.optimize import minimize
 from spring_mass import spring_mass
 
-def kalman_filter(observed_distance, time_intervals, process_variance, measurement_variance, initial_state):
-    initial_distance = initial_state[0]
-    initial_velocity = initial_state[1]
-    x = np.array([[initial_distance], [initial_velocity]])  # State vector [distance, velocity]
-    P = np.diag([1, 1])  # Initial covariance matrix
+def kalman_filter(observed_distance, delta_t, process_variance, measurement_variance, x, P):
+    # x is the initial state with [initial distance, intial velocity]
     H = np.array([[1, 0]])  # Measurement matrix
     R = np.array([[measurement_variance]])  # Measurement covariance
-    estimated_distance = []
-    for i in range(len(observed_distance)):
-        # Prediction step
-        delta_t = time_intervals[i-1] if i > 0 else 1.0  # Use 1.0 as a default for the first iteration
-        F = np.array([[1, delta_t], [0, 1]])  # State transition matrix
-        Q = np.array([[0.5 * delta_t**2 * process_variance, delta_t * process_variance],
-                      [delta_t * process_variance, process_variance]])  # Process covariance
-        x_hat = np.dot(F, x)
-        P_hat = np.dot(np.dot(F, P), F.T) + Q
-        # Update step
-        y = observed_distance[i] - np.dot(H, x_hat)
-        S = np.dot(np.dot(H, P_hat), H.T) + R
-        K = np.dot(np.dot(P_hat, H.T), np.linalg.inv(S))
-        x = x_hat + np.dot(K, y)
-        P = np.dot((np.eye(2) - np.dot(K, H)), P_hat)
-        # Save the estimated distance
-        estimated_distance.append(x[0, 0])
+    # Prediction step
+    F = np.array([[1, delta_t], [0, 1]])  # State transition matrix
+    Q = np.array([[0.5 * delta_t**2 * process_variance, delta_t * process_variance],
+                    [delta_t * process_variance, process_variance]])  # Process covariance
+    x_hat = np.dot(F, x)
+    P_hat = np.dot(np.dot(F, P), F.T) + Q
+    # Update step
+    y = observed_distance[i] - np.dot(H, x_hat)
+    S = np.dot(np.dot(H, P_hat), H.T) + R
+    K = np.dot(np.dot(P_hat, H.T), np.linalg.inv(S))
+    x = x_hat + np.dot(K, y)
+    P = np.dot((np.eye(2) - np.dot(K, H)), P_hat)
 
-    return estimated_distance
+    return x, P
 
-optimize = False
+optimize = True
 np.random.seed(42)
 true_distance, time_intervals = spring_mass()
+time_intervals.insert(0, 1.0) # # Use 1.0 as a default for the first iteration
 # true_distance = np.linspace(0, 100, 100)
 observed_distance = true_distance + np.random.normal(0, 5, len(true_distance))
 # time_intervals = np.random.uniform(0.5, 1.5, 100)
-initial_state = [observed_distance[0], 0.0] # [initial distance, initial relative velocity]
+initial_state = np.array([observed_distance[0], 0.0]) # [initial distance, initial relative velocity]
+initial_P = np.diag([1, 1])
 def objective_function(params):
     process_variance, measurement_variance = params
-    estimated_distance = kalman_filter(observed_distance, time_intervals, process_variance, measurement_variance, initial_state)
+    estimated_distance = []
+    state = initial_state
+    P = initial_P
+    for i in range(len(observed_distance)):
+        state, P = kalman_filter(observed_distance, time_intervals[i], process_variance, measurement_variance, state, P)
+        estimated_distance.append(state[0])
     mse = np.mean((true_distance - estimated_distance)**2)
     return mse
 
+
 initial_guess = [0.1, 5.0]
-bounds = [(1e-5, 1.0), (1e-5, 10.0)]
-estimated_distance_unoptimized = kalman_filter(observed_distance, time_intervals, initial_guess[0], initial_guess[1], initial_state)
+bounds = [(1e-5, 1), (1.0, 50)]
+state = initial_state
+P = initial_P  # Initial covariance matrix
+estimated_distance_unoptimized = []
+for i in range(len(observed_distance)):
+    state, P = kalman_filter(observed_distance, time_intervals[i], initial_guess[0], initial_guess[1], state, P)
+    estimated_distance_unoptimized.append(state[0])
 if optimize:
     result = minimize(objective_function, initial_guess, bounds=bounds)
     best_process_variance, best_measurement_variance = result.x
-    estimated_distance_optimized = kalman_filter(observed_distance, time_intervals, best_process_variance, best_measurement_variance, initial_state)
+    state = initial_state
+    P = initial_P
+    estimated_distance_optimized = []
+    for i in range(len(observed_distance)):
+        state, P = kalman_filter(observed_distance, time_intervals[i], best_process_variance, best_measurement_variance, state, P)
+        estimated_distance_optimized.append(state[0])
+
+mse_observed = np.mean((true_distance - observed_distance)**2)
+mse_unoptimized = np.mean((true_distance - estimated_distance_unoptimized)**2)
+print("\nMean Squared Error (Observed Distance):", mse_observed)
+print("Mean Squared Error (Unoptimized Estimated Distance):", mse_unoptimized)
+if optimize:
+    mse_optimized = np.mean((true_distance - estimated_distance_optimized)**2)
+    print("Mean Squared Error (Optimized Estimated Distance):", mse_optimized)
+    print("Best Process Variance (Optimized):", best_process_variance)
+    print("Best Measurement Variance (Optimized):", best_measurement_variance)
+
 plt.figure(figsize=(12, 6))
 if optimize:
     plt.subplot(1, 2, 1)
@@ -70,12 +91,3 @@ plt.ylabel('Distance')
 plt.title('Unoptimized Kalman Filter for Distance Estimation')
 plt.tight_layout()
 plt.show()
-mse_observed = np.mean((true_distance - observed_distance)**2)
-mse_unoptimized = np.mean((true_distance - estimated_distance_unoptimized)**2)
-print("\nMean Squared Error (Observed Distance):", mse_observed)
-print("Mean Squared Error (Unoptimized Estimated Distance):", mse_unoptimized)
-if optimize:
-    print("Best Process Variance (Optimized):", best_process_variance)
-    print("Best Measurement Variance (Optimized):", best_measurement_variance)
-    mse_optimized = np.mean((true_distance - estimated_distance_optimized)**2)
-    print("Mean Squared Error (Optimized Estimated Distance):", mse_optimized)
