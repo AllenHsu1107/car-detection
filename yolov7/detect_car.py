c2622cf144b9c8b7fb82075a6089a967dc2d1941
AllenHsu1107 <126022146+AllenHsu1107@users.noreply.github.com>
2023-11-26 00:16:15 -0800

    Add files via upload

---
+++ yolov7/detect_car.py
@@ -0,0 +1,352 @@
+import argparse
+import time
+from pathlib import Path
+
+import cv2
+import torch
+import torch.backends.cudnn as cudnn
+import numpy as np
+from numpy import random
+
+from models.experimental import attempt_load
+from utils.datasets import LoadStreams, LoadImages
+from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
+    scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
+from utils.plots import plot_one_box
+from utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
+
+from kalman_filter import kalman_filter, objective_function
+from spring_mass import spring_mass
+from scipy.optimize import minimize
+
+DISTANCE = 3
+initial_guess = [0.1, 5.0]
+bounds = [(1e-5, 1), (1.0, 50)]
+state = np.array([1, 0.0])
+P = np.diag([1, 1])
+
+def grayscale(image):
+  image=np.asarray(image)
+  return cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
+
+def gauss(image):
+  return cv2.GaussianBlur(image,(5,5),0)
+
+def canny(image):
+    edges = cv2.Canny(image,50,150)
+    return edges
+
+def region(image):
+    height, width = image.shape
+    triangle = np.array([
+                       [(int(width/4), height), (int(width/2), int(height*5/9)), (int(width*5/6), height)]
+                       ])
+    
+    mask = np.zeros_like(image)
+    mask = cv2.fillPoly(mask, triangle, 255)
+    mask = cv2.bitwise_and(image, mask)
+    return mask
+
+def average(image, lines):
+    left = []
+    right = []
+    if lines is None:
+        left_line = make_points(image, 0)
+        right_line = make_points(image, 0)
+    else:
+        for line in lines:
+            x1, y1, x2, y2 = line.reshape(4)
+            parameters = np.polyfit((x1, x2), (y1, y2), 1)
+            slope = parameters[0]
+            y_int = parameters[1]
+            if slope < 0:
+                left.append((slope, y_int))
+            else:
+                right.append((slope, y_int))
+
+        left_avg = np.average(left, axis=0)
+        right_avg = np.average(right, axis=0)
+        left_line = make_points(image, left_avg)
+        right_line = make_points(image, right_avg)
+
+    return np.array([left_line, right_line])
+
+def make_points(image, average):
+    try:
+        slope, y_int = average
+    except TypeError:
+        slope, y_int = 1, 0
+    if slope == 0:
+        slope = 1
+    y1 = image.shape[0]
+    y2 = int(y1 * (3/5))
+    x1 = int((y1 - y_int) // slope)
+    x2 = int((y2 - y_int) // slope)
+    return np.array([x1, y1, x2, y2])
+
+def display_lines(image, lines):
+    lines_image = np.zeros_like(image)
+    if lines is not None and lines.shape[0] == 2:
+        for line in lines:
+            x1, y1, x2, y2 = line
+            #print(line)
+            try:
+                cv2.line(lines_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
+            except Exception as Argument:
+                pass
+
+    return lines_image
+
+def detect(save_img=False):
+    source, weights, view_img, save_txt, imgsz, trace = opt.source, opt.weights, opt.view_img, opt.save_txt, opt.img_size, not opt.no_trace
+    save_img = not opt.nosave and not source.endswith('.txt')  # save inference images
+    webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
+        ('rtsp://', 'rtmp://', 'http://', 'https://'))
+
+    # Directories
+    save_dir = Path(increment_path(Path(opt.project) / opt.name, exist_ok=opt.exist_ok))  # increment run
+    (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
+
+    # Initialize
+    set_logging()
+    device = select_device(opt.device)
+    half = device.type != 'cpu'  # half precision only supported on CUDA
+
+    # Load model
+    model = attempt_load(weights, map_location=device)  # load FP32 model
+    stride = int(model.stride.max())  # model stride
+    imgsz = check_img_size(imgsz, s=stride)  # check img_size
+
+    if trace:
+        model = TracedModel(model, device, opt.img_size)
+
+    if half:
+        model.half()  # to FP16
+
+    # Second-stage classifier
+    classify = False
+    if classify:
+        modelc = load_classifier(name='resnet101', n=2)  # initialize
+        modelc.load_state_dict(torch.load('weights/resnet101.pt', map_location=device)['model']).to(device).eval()
+
+    # Set Dataloader
+    vid_path, vid_writer = None, None
+    if webcam:
+        view_img = check_imshow()
+        cudnn.benchmark = True  # set True to speed up constant image size inference
+        dataset = LoadStreams(source, img_size=imgsz, stride=stride)
+    else:
+        dataset = LoadImages(source, img_size=imgsz, stride=stride)
+
+    # Get names and colors
+    names = model.module.names if hasattr(model, 'module') else model.names
+    colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
+
+    # Run inference
+    if device.type != 'cpu':
+        model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
+    old_img_w = old_img_h = imgsz
+    old_img_b = 1
+
+    t0 = time.time()
+
+    for path, img, im0s, vid_cap in dataset:
+        img = torch.from_numpy(img).to(device)
+        img = img.half() if half else img.float()  # uint8 to fp16/32
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+        if img.ndimension() == 3:
+            img = img.unsqueeze(0)
+        
+        # Warmup
+        if device.type != 'cpu' and (old_img_b != img.shape[0] or old_img_h != img.shape[2] or old_img_w != img.shape[3]):
+            old_img_b = img.shape[0]
+            old_img_h = img.shape[2]
+            old_img_w = img.shape[3]
+            for i in range(3):
+                model(img, augment=opt.augment)[0]
+
+        # Inference
+        t1 = time_synchronized()
+        with torch.no_grad():   # Calculating gradients would cause a GPU memory leak
+            pred = model(img, augment=opt.augment)[0]
+        t2 = time_synchronized()
+
+        # Apply NMS
+        pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
+        t3 = time_synchronized()
+
+        # Apply Classifier
+        if classify:
+            pred = apply_classifier(pred, modelc, img, im0s)
+
+        # Process detections
+        for i, det in enumerate(pred):  # detections per image
+            if webcam:  # batch_size >= 1
+                p, s, im0, frame = path[i], '%g: ' % i, im0s[i].copy(), dataset.count
+            else:
+                p, s, im0, frame = path, '', im0s, getattr(dataset, 'frame', 0)
+
+            # Process lane detection
+            gray = grayscale(im0)
+            blur = gauss(gray)
+
+            ### Yellow
+            img_hsv = cv2.cvtColor(im0, cv2.COLOR_RGB2HSV)
+            lower_yellow = np.array([20, 100, 100], dtype = 'uint8')
+            upper_yellow = np.array([30, 255, 255], dtype = 'uint8')
+
+            mask_yellow = cv2.inRange(img_hsv, lower_yellow, upper_yellow)
+            #mask_white = cv2.inRange(gray, np.mean(gray)*1.15, 255) # NEED TO ADJUST VALUE
+            mask_white = cv2.inRange(gray, 200, 255) # NEED TO ADJUST VALUE
+            mask_yw = cv2.bitwise_or(mask_white, mask_yellow)
+            mask_yw_image = cv2.bitwise_and(gray, mask_yw)
+            ###
+
+            edges = canny(mask_yw_image)
+            mask = region(edges)
+
+            lines = cv2.HoughLinesP(mask, 2, np.pi/180, 120, np.array([]), minLineLength=40, maxLineGap=5)
+            averaged_lines = average(im0, lines)
+            black_lines = display_lines(im0, averaged_lines)
+
+            slope = np.ones(2)
+            offset = np.zeros(2)
+            count = 0
+        
+            if averaged_lines is not None and averaged_lines.shape[0] == 2:
+                for line in averaged_lines:
+                    x1, y1, x2, y2 = line
+                    slope[count] = (y2 - y1)/(x2 - x1)
+                    offset[count] = y1 - slope[count]*x1
+                    count += 1
+            
+            p = Path(p)  # to Path
+            save_path = str(save_dir / p.name)  # img.jpg
+            txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # img.txt
+            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+            if len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+
+                # Print results
+                for c in det[:, -1].unique():
+                    n = (det[:, -1] == c).sum()  # detections per class
+                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+
+                closest_bound1 = 0
+                closest_bound2 = im0.shape[1]
+                closest_y = 0
+
+                # Write results
+                for *xyxy, conf, cls in reversed(det):
+                    if save_txt:  # Write to file
+                        xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+                        line = (cls, *xywh, conf) if opt.save_conf else (cls, *xywh)  # label format
+                        with open(txt_path + '.txt', 'a') as f:
+                            f.write(('%g ' * len(line)).rstrip() % line + '\n')
+
+                    if save_img or view_img:  # Add bbox to image
+                        label = f'{names[int(cls)]} {conf:.2f}'
+                        plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=1)
+
+                        if f'{names[int(cls)]}' == 'car' or f'{names[int(cls)]}' == 'truck':
+                            x1, y1, x2, y2 = xyxy
+                            bound1 = int((y2 - offset[0])/slope[0])
+                            bound2 = int((y2 - offset[1])/slope[1])
+                            if (x1 >= bound1 and x1 <= bound2) or (x2 >= bound1 and x2 <= bound2):
+                                if (y2 > closest_y):
+                                    closest_y = y2
+                                    closest_bound1 = bound1
+                                    closest_bound2 = bound2
+
+                # Calculation of distance (ratio)
+                if (closest_y != 0):
+                    pos_wid = closest_bound2 - closest_bound1
+                    bot_wid = (im0.shape[0] - offset[1])/slope[1] - (im0.shape[0] - offset[0])/slope[0]
+                    dis = DISTANCE * bot_wid/pos_wid 
+
+                else:
+                    dis = 0
+                    print("No Car Detected")
+                
+                print("Raw Distance: ", dis)
+
+                #result = minimize(objective_function, initial_guess, bounds=bounds)
+                #best_process_variance, best_measurement_variance = result.x
+                best_process_variance = initial_guess[0]
+                best_measurement_variance = initial_guess[1]
+                global state, P
+                state, P = kalman_filter(dis, 1.0, best_process_variance, best_measurement_variance, state, P)
+                estimated_distance_optimized = state[0]
+
+                print("Optimized Distance: ", estimated_distance_optimized)
+
+            # Print time (inference + NMS)
+            print(f'{s}Done. ({(1E3 * (t2 - t1)):.1f}ms) Inference, ({(1E3 * (t3 - t2)):.1f}ms) NMS')
+
+            # Add lanes to img
+            im0 = cv2.addWeighted(im0, 0.8, black_lines, 1, 1)
+
+            # Stream results
+            if view_img:
+                cv2.imshow(str(p), im0)
+                cv2.waitKey(1)  # 1 millisecond
+
+            # Save results (image with detections)
+            if save_img:
+                if dataset.mode == 'image':
+                    cv2.imwrite(save_path, im0)
+                    print(f" The image with the result is saved in: {save_path}")
+                else:  # 'video' or 'stream'
+                    if vid_path != save_path:  # new video
+                        vid_path = save_path
+                        if isinstance(vid_writer, cv2.VideoWriter):
+                            vid_writer.release()  # release previous video writer
+                        if vid_cap:  # video
+                            fps = vid_cap.get(cv2.CAP_PROP_FPS)
+                            w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                            h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                        else:  # stream
+                            fps, w, h = 30, im0.shape[1], im0.shape[0]
+                            save_path += '.mp4'
+                        vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
+                    vid_writer.write(im0)
+
+    if save_txt or save_img:
+        s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
+        #print(f"Results saved to {save_dir}{s}")
+
+    print(f'Done. ({time.time() - t0:.3f}s)')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', nargs='+', type=str, default='yolov7-tiny.pt', help='model.pt path(s)')
+    parser.add_argument('--source', type=str, default='inference/images', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
+    parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--view-img', action='store_true', help='display results')
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    parser.add_argument('--update', action='store_true', help='update all models')
+    parser.add_argument('--project', default='runs/detect', help='save results to project/name')
+    parser.add_argument('--name', default='exp', help='save results to project/name')
+    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+    parser.add_argument('--no-trace', action='store_true', help='don`t trace model')
+    opt = parser.parse_args()
+    print(opt)
+    #check_requirements(exclude=('pycocotools', 'thop'))
+
+    with torch.no_grad():
+        if opt.update:  # update all models (to fix SourceChangeWarning)
+            for opt.weights in ['yolov7.pt']:
+                detect()
+                strip_optimizer(opt.weights)
+        else:
+            detect()
