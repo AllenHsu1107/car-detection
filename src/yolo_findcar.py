a597fda495e816c4ebe321b5b743d9ba0d50ad28
tim701-study <84507954+tim701-study@users.noreply.github.com>
2023-12-06 11:25:54 -0800

    Update yolo_findcar.py

--- src/yolo_findcar.py
+++ src/yolo_findcar.py
@@ -1,14 +1,15 @@
 import cv2
 from ultralytics import YOLO
 
+model = YOLO('yolov8n.pt')  # load a pretrained YOLOv8n detection model
+
 pixelx = 800
 pixely = 600
-
-model = YOLO('yolov8n.pt')  # load a pretrained YOLOv8n detection model
 
 objName = 'car'
 
 def DetectObject(objName,img):
+    py,px = img.shape[0],img.shape[1]
     output = model(img,verbose=False)
     first_output = output[0]
     names = first_output.names
@@ -16,13 +17,15 @@ def DetectObject(objName,img):
         if names[int(i.cls)] == objName:
             x1,y1,x2,y2 = int(i.xyxy[0][0]),int(i.xyxy[0][1]),int(i.xyxy[0][2]),int(i.xyxy[0][3])
             xmid, ymid = (x1+x2)/2,(y1+y2)/2
-            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 3)
-    return xmid,ymid
+            if ((ymid/xmid < (py/px*8/3-2/3*py/xmid)) and (ymid/xmid < (-py/px*8/3+2*py/xmid)) and (ymid < 3/4*py)):
+                cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 3)
+                finalx,finaly = (x1+x2)/2,(y1+y2)/2
+    return finalx,finaly
 
-
-path = 'example/GT_left (14).jpg'
+path = 'example/GT_left (6).jpg'
 frame = cv2.imread(path)
+x,y = DetectObject(objName,frame)
+print(x,y)
 frame = cv2.resize(frame,(pixelx,pixely))
-carx,cary = DetectObject(objName,frame)
 cv2.imshow('ObjectDetection',frame)
 cv2.waitKey(0)
