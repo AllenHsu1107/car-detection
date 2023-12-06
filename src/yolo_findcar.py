71edca5ca0b3ded4f459ea537802dc3548fd9051
tim701-study <84507954+tim701-study@users.noreply.github.com>
2023-12-05 22:16:36 -0800

    Create yolo_findcar.py

---
+++ src/yolo_findcar.py
@@ -0,0 +1,28 @@
+import cv2
+from ultralytics import YOLO
+
+pixelx = 800
+pixely = 600
+
+model = YOLO('yolov8n.pt')  # load a pretrained YOLOv8n detection model
+
+objName = 'car'
+
+def DetectObject(objName,img):
+    output = model(img,verbose=False)
+    first_output = output[0]
+    names = first_output.names
+    for i in first_output.boxes:
+        if names[int(i.cls)] == objName:
+            x1,y1,x2,y2 = int(i.xyxy[0][0]),int(i.xyxy[0][1]),int(i.xyxy[0][2]),int(i.xyxy[0][3])
+            xmid, ymid = (x1+x2)/2,(y1+y2)/2
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 3)
+    return xmid,ymid
+
+
+path = 'example/GT_left (14).jpg'
+frame = cv2.imread(path)
+carx,cary = DetectObject(objName,frame)
+frame = cv2.resize(frame,(pixelx,pixely))
+cv2.imshow('ObjectDetection',frame)
+cv2.waitKey(0)
